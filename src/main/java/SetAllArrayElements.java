466e418e0f441a56200064f69b49619ae32f819d
Revathi-Kamineni <revathikamineni77@gmail.com>
2023-01-23 15:46:33 +0000

    SetAllArrayElements

--- src/main/java/SetAllArrayElements.java
+++ src/main/java/SetAllArrayElements.java
@@ -23,6 +23,16 @@ public class SetAllArrayElements {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
+        for(int i=0; i<arr.length;i++){  
+            arr[i]=n;
+           System.out.println(arr[i]);
+       }
+    }
+    public static void main(String[] args){
+        int[] arr={1,2,3,4,5};
+
+        SetAllArrayElements s = new SetAllArrayElements();     
+               s.set(arr, 0);
 
     }
 }
